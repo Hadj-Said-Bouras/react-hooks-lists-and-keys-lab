c62317d8bc7648c533abeff6fe7e606bb90ca610
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-09-10 21:54:57 +0100

    Completed assigment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,33 @@
+// import React from "react";
+// import ProjectItem from "./ProjectItem";
+
+// function ProjectList({ projects }) {
+//   console.log(projects);
+//   const list = projects.map((project) => {
+//     console.log(project)
+//   })
+//   return (
+//     <div id="projects">
+//       <h2>My Projects</h2>
+//       <div id="project-list">
+//           <ProjectItem technologies={projects} /> 
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectList;
+
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const ProjectItems = projects.map((project, id) => <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies} />);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{ProjectItems}</div>
     </div>
   );
 }
